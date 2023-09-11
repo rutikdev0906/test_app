df35af34789b64733d6dd985ce0f315bdfecac48
rutikdev0906 <rutikparmar445@gmail.com>
2023-09-11 11:30:16 +0530

    New Update

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // loaders: {
-  //   // ...
-  //   'scss': ['vue-style-loader', 'css-loader', 'sass-loader'],
-  // },
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 
-// import { fileURLToPath, URL } from "url";
 
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "url";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       "@": fileURLToPath(new URL("./src", import.meta.url)),
-//     },
-//   },
-// });
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
